079dcc1d143d872bdbf67da7a484b4c64d7d8a45
Surinder Singh <php@nekinsan.com>
2024-10-04 18:22:45 +0530

    update name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Nekinsan | Order Management') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
